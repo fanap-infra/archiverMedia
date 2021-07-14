ede0c1239bf903cc3a1548e090ed029810d9c5b4
Saeid Aghapour <saeid.aghapour@gmail.com>
2021-07-14 14:19:50 +0430

    rfc/events
    - VirtualFileDeleted

--- events.go
+++ events.go
@@ -3,3 +3,8 @@ package archiverMedia
 type Events interface {
 	DeleteFile(fileID uint32)
 }
+
+func (arch *Archiver) VirtualFileDeleted(fileID uint32, message string) {
+	arch.log.Warnv("Media file deleted", "fileID", fileID, "message", message)
+	arch.EventsHandler.DeleteFile(fileID)
+}
